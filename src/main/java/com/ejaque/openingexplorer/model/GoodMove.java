0e1c383ebec79046fe31e5144216bfb46a31a309
Emilio Jaque <ejaque74@hotmail.com>
2023-12-26 01:56:01 -0300

    Added export to excel file

--- src/main/java/com/ejaque/openingexplorer/model/GoodMove.java
+++ src/main/java/com/ejaque/openingexplorer/model/GoodMove.java
@@ -5,6 +5,27 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * Represents a good chess move identified during move analysis. This class encapsulates 
+ * various metrics and characteristics of a chess move that are used to determine its efficacy 
+ * in a given chess position.
+ * 
+ * <p>This class includes metrics such as probability of occurrence, rating rank, rating percentile, 
+ * average rating of the move, and average rating for all moves in a given position.</p>
+ * 
+ * <p>Attributes:</p>
+ * <ul>
+ *     <li>{@code fen} - The FEN (Forsyth-Edwards Notation) string representing the chess board position for this move.</li>
+ *     <li>{@code move} - The move in UCI (Universal Chess Interface) format.</li>
+ *     <li>{@code probabilityOcurring} - The probability of this move occurring in the given board position.</li>
+ *     <li>{@code ratingRank} - The rank of this move based on the average rating of players who played it.</li>
+ *     <li>{@code ratingPercentile} - The percentile standing of this move in terms of player ratings.</li>
+ *     <li>{@code averageRating} - The average rating of players who have played this move.</li>
+ *     <li>{@code averageRatingForAllMoves} - The average rating for all moves in the given board position.</li>
+ * </ul>
+ *
+ * <p>Utilizes Lombok annotations for boilerplate code like getters, setters, builder, and toString method.</p>
+ */
 @Builder
 @Setter
 @Getter
---
+++ src/main/java/com/ejaque/openingexplorer/service/ExcelExportService.java
@@ -0,0 +1,69 @@
+package com.ejaque.openingexplorer.service;
+
+import com.ejaque.openingexplorer.model.GoodMove;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ExcelExportService {
+
+    @Value("${output.file.path}")
+    private String excelFilePath;
+
+    public void generateExcel(List<GoodMove> goodMoves) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Good Moves");
+
+        createHeaderRow(sheet);
+
+        int rowCount = 1;
+        for (GoodMove move : goodMoves) {
+            Row row = sheet.createRow(rowCount++);
+            writeGoodMove(move, row);
+        }
+
+        try (FileOutputStream outputStream = new FileOutputStream(excelFilePath)) {
+            workbook.write(outputStream);
+        }
+    }
+
+    private void createHeaderRow(Sheet sheet) {
+        Row headerRow = sheet.createRow(0);
+
+        headerRow.createCell(0).setCellValue("FEN");
+        headerRow.createCell(1).setCellValue("Move");
+        headerRow.createCell(2).setCellValue("Probability Occurring");
+        headerRow.createCell(3).setCellValue("Rating Rank");
+        headerRow.createCell(4).setCellValue("Rating Percentile");
+        headerRow.createCell(5).setCellValue("Average Rating");
+        headerRow.createCell(6).setCellValue("Average Rating For All Moves");
+    }
+
+    private void writeGoodMove(GoodMove move, Row row) {
+        Cell cell = row.createCell(0);
+        cell.setCellValue(move.getFen());
+
+        cell = row.createCell(1);
+        cell.setCellValue(move.getMove());
+
+        cell = row.createCell(2);
+        cell.setCellValue(move.getProbabilityOcurring());
+
+        cell = row.createCell(3);
+        cell.setCellValue(move.getRatingRank());
+
+        cell = row.createCell(4);
+        cell.setCellValue(move.getRatingPercentile());
+
+        cell = row.createCell(5);
+        cell.setCellValue(move.getAverageRating());
+
+        cell = row.createCell(6);
+        cell.setCellValue(move.getAverageRatingForAllMoves());
+    }
+}

abc00ad549ea13faa481abb6900c2d160c9d9a0e
Emilio Jaque <ejaque74@hotmail.com>
2024-01-07 15:55:17 -0300

    MILESTONE: now we have the moves coorectly MARKED

--- src/main/java/com/ejaque/openingexplorer/service/AnnotatedPgnMarkerService.java
+++ src/main/java/com/ejaque/openingexplorer/service/AnnotatedPgnMarkerService.java
@@ -28,35 +28,52 @@ public class AnnotatedPgnMarkerService {
         StringBuilder annotatedPgn = new StringBuilder();
         for (Game game : pgn.getGames()) {
             Board board = new Board();
-            String[] moves = game.getHalfMoves().toString().split("\\s+");
+            MoveList moveList = game.getHalfMoves();
+            String[] moves = moveList.toString().split("\\s+");
             Map<Integer, Map<Integer, MoveList>> gameToVariationsMap = getGameToVariationsMap(game);
 //            boolean nextMoveIsImportant = false;
 
         	int moveCounter = 0;
+        	int moveIndex = 0;
             for (String moveStr : moves) {
 
             	moveCounter++;
+            	
+            	// the last move is not relevant, as we will never mark this
+            	if (moveCounter == moveList.size()) continue;
             	
             	// add the move number
             	if ((moveCounter - 1) % 2 == 0) {
             		annotatedPgn.append((moveCounter - 1) / 2 + 1 + ". ");
             	}
             	
-            	int totalVariations = Optional.ofNullable(gameToVariationsMap.get(moveCounter))
+            	int totalVariationsNextMove = Optional.ofNullable(gameToVariationsMap.get(moveCounter + 1))
                         .map(variations -> variations.size())
                         .orElse(0);
-				log.debug("TOTAL: moveCounter={} totalVariations={}", moveCounter, totalVariations);
             	
-            	if (totalVariations > 0 || isImportantMove(moveStr)) {
+            	String moveSan = "";
+            	String nextMoveSan = "";
+            	
+            	if (moveCounter > 1) {
+            		nextMoveSan = moveList.get(moveCounter).getSan();
+            	}
+            	
+            	if (moveCounter < moveList.size()) {
+            		moveSan = moveList.get(moveCounter - 1).getSan();
+            	}
+            	
+            	log.debug("TOTAL: moveCounter={} moveSan={} totalVariations={}", moveCounter, moveSan, totalVariationsNextMove);
+            	
+            	if (totalVariationsNextMove > 0 || isMoveWithSymbol(nextMoveSan)) {
                     Move move = new Move(moveStr, board.getSideToMove());
                     board.doMove(move);
-                    annotatedPgn.append(moveStr)
+                    annotatedPgn.append(moveSan)
                                  .append(" { [%csl R")
                                  .append(getDestinationSquare(move))
                                  .append("]} ");
                 } else {
                     board.doMove(new Move(moveStr, board.getSideToMove()));
-                    annotatedPgn.append(moveStr).append(" ");
+                    annotatedPgn.append(moveSan).append(" ");
                 }
             }
             annotatedPgn.append("\n\n");
@@ -83,7 +100,14 @@ public class AnnotatedPgnMarkerService {
             if (game.getVariations() != null) {
                 MoveList var = game.getVariations().get(variantIndex);
                 if (var != null) {
-                	Map<Integer, MoveList> variationsMap = new HashMap<>();  
+                	Map<Integer, MoveList> variationsMap = new HashMap<>();
+                	
+                	MoveList child = game.getVariations().get(variantIndex);
+                	
+                	// add current variation
+                	variationsMap.put(variantIndex, child);
+                	
+                	// add nested variations
                     variantIndex = translateVariation(variationsMap, game, var, -1,
                             variantIndex, index, moveCounter);
                     
@@ -156,7 +180,7 @@ public class AnnotatedPgnMarkerService {
 	}
     
     
-    private boolean isImportantMove(String moveStr) {
+    private boolean isMoveWithSymbol(String moveStr) {
         return moveStr.contains("!") || moveStr.contains("?");
     }
 
